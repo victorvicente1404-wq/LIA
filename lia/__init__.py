41ebb5b3b23047ebce1d063faf9befea97ecd4fd
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-16 14:46:36 -0300

    Refactor Lia class import to use assistente module

--- lia/__init__.py
+++ lia/__init__.py
@@ -1,14 +1,1 @@
-from .interpretador import Interpretador
-from .memoria import Memoria
-
-
-class Lia:
-
-    def __init__(self):
-
-        self.interpretador = Interpretador()
-        self.memoria = Memoria()
-
-    def iniciar(self):
-
-        ...
+from .assistente import Lia
