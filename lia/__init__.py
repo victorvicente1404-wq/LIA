e98001399b713c73d7e8ce07a9ed3e9f6681e898
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-16 14:40:05 -0300

    Refactor Lia class to use Interpretador and Memoria

--- lia/__init__.py
+++ lia/__init__.py
@@ -1,1 +1,14 @@
-from .assistente import Lia
+from .interpretador import Interpretador
+from .memoria import Memoria
+
+
+class Lia:
+
+    def __init__(self):
+
+        self.interpretador = Interpretador()
+        self.memoria = Memoria()
+
+    def iniciar(self):
+
+        ...
