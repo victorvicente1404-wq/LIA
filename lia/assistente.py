2cb955627e91b0b25a41f9663b1ca43f5eb7e64f
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-16 14:18:26 -0300

    Add text normalization method in assistente.py

--- lia/assistente.py
+++ lia/assistente.py
@@ -3,6 +3,25 @@ class Lia:
     def __init__(self):
         self.nome_usuario = ""
 
+    def normalizar(self, texto):
+
+        texto = texto.lower()
+
+        texto = texto.replace("é", "e")
+        texto = texto.replace("ê", "e")
+        texto = texto.replace("á", "a")
+        texto = texto.replace("à", "a")
+        texto = texto.replace("ã", "a")
+        texto = texto.replace("â", "a")
+        texto = texto.replace("í", "i")
+        texto = texto.replace("ó", "o")
+        texto = texto.replace("ô", "o")
+        texto = texto.replace("õ", "o")
+        texto = texto.replace("ú", "u")
+        texto = texto.replace("ç", "c")
+
+        return texto
+
     def iniciar(self):
 
         print("Lia: Ola!")
@@ -13,7 +32,15 @@ class Lia:
 
         pergunta = input("Voce: ")
 
-        if pergunta.lower() == "qual e meu nome?":
+        pergunta = self.normalizar(pergunta)
+
+        if (
+            "meu nome" in pergunta
+            or "quem eu sou" in pergunta
+        ):
+
             print(f"Lia: Seu nome e {self.nome_usuario}.")
+
         else:
+
             print("Lia: Ainda estou aprendendo.")
