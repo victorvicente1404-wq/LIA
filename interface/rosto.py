dd7166aba90ee1fe74bad7d8ff177b9e59dae0cd
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-17 09:51:31 -0300

    Implement Rosto class with various expressions

--- interface/rosto.py
+++ interface/rosto.py
@@ -1,1 +1,57 @@
+class Rosto:
 
+    def __init__(self):
+
+        self.olho_esquerdo = "◕"
+        self.olho_direito = "◕"
+        self.boca = "◡"
+
+    def normal(self):
+
+        self.olho_esquerdo = "◕"
+        self.olho_direito = "◕"
+        self.boca = "◡"
+
+    def feliz(self):
+
+        self.olho_esquerdo = "^"
+        self.olho_direito = "^"
+        self.boca = "◡"
+
+    def pensando(self):
+
+        self.olho_esquerdo = "•"
+        self.olho_direito = "•"
+        self.boca = "-"
+
+    def surpresa(self):
+
+        self.olho_esquerdo = "○"
+        self.olho_direito = "○"
+        self.boca = "O"
+
+    def triste(self):
+
+        self.olho_esquerdo = "◕"
+        self.olho_direito = "◕"
+        self.boca = "︵"
+
+    def piscar(self):
+
+        self.olho_esquerdo = "^"
+        self.olho_direito = "◕"
+        self.boca = "◡"
+
+    def falar(self):
+
+        self.olho_esquerdo = "◕"
+        self.olho_direito = "◕"
+        self.boca = "○"
+
+    def obter(self):
+
+        return (
+            f"({self.olho_esquerdo} "
+            f"{self.boca} "
+            f"{self.olho_direito})"
+        )
