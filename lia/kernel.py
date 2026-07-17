80dcfa7750534f402903952686dea794ba5fa0a2
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-17 13:46:54 -0300

    Update kernel.py

--- lia/kernel.py
+++ lia/kernel.py
@@ -1,1 +1,33 @@
+"""
+Kernel da Lia.
 
+Este módulo coordena toda a assistente.
+
+Nenhum outro módulo deve controlar diretamente
+a aplicação. Toda comunicação passa pelo Kernel.
+"""
+
+from .assistente import Assistente
+
+
+class Kernel:
+
+    def __init__(self):
+
+        self.assistente = Assistente()
+
+        self.iniciado = False
+
+    def iniciar(self):
+
+        if self.iniciado:
+
+            return
+
+        self.iniciado = True
+
+    def responder(self, mensagem):
+
+        return self.assistente.responder(
+            mensagem
+        )
