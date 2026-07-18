b71773615e2068fa61aa4482baa62d3be8222253
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-18 15:18:24 -0300

    Update __init__.py

--- interface/__init__.py
+++ interface/__init__.py
@@ -5,11 +5,22 @@ Interface gráfica da Lia.
 from .janela import Janela
 from .rosto import Rosto
 from .chat import Chat
+from .componentes import (
+    BotaoLia, 
+    EntradaLia, 
+    TituloLia, 
+    Painel
+)
 from .tema import Tema
 
 __all__ = [
     "Janela",
     "Rosto",
     "Chat",
+    "BotaoLia",
+    "EntradaLia",
+    "TituloLia",
+    "Painel",
     "Tema"
 ]
+
