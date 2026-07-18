5ffe8b05e26b6e27b589618cecf37af546036495
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-18 14:50:42 -0300

    Update __init__.py

--- ferramentas/__init__.py
+++ ferramentas/__init__.py
@@ -1,1 +1,18 @@
+"""
+Pacote de ferramentas da Lia.
+Aqui são importadas todas as ferramentas disponíveis.
+"""
 
+from .pesquisa import Pesquisa
+from .internet import Internet
+from .calculadora import Calculadora
+from .sistema import Sistema
+from .tempo import Tempo
+
+__all__ = [
+    "Pesquisa",
+    "Internet",
+    "Calculadora",
+    "Sistema",
+    "Tempo"
+]
